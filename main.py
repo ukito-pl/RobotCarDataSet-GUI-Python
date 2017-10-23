07a5f40e23e9eb50556e4a28c0cce929228a0e65
Marek <bulabot@gmail.com>
2017-10-23 15:07:30 +0200

    Wybór danych

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ import pyqtgraph.opengl as gl
 import pyqtgraph as pg
 import sys
 import MainWindowDesign
+import SelectDataWindowDesign
 from buildPointCloudThread import buildPointcloudThread
 import numpy as np
 
 
+
+class SelectDataWindow(QtGui.QDialog, SelectDataWindowDesign.Ui_Dialog):
+    def __init__(self):
+        # Using super allows us to
+        # access variables, methods etc in the MainWindowDesign.py file
+        super(self.__class__, self).__init__()
+        self.setupUi(self)  # This is defined in MainWindowDesign.py file automatically
+                            # It sets up layout and widgets that are defined
 
 
 class Application(QtGui.QMainWindow, MainWindowDesign.Ui_MainWindow):
@@ -20,14 +29,37 @@ class Application(QtGui.QMainWindow, MainWindowDesign.Ui_MainWindow):
         self.setupUi(self)  # This is defined in MainWindowDesign.py file automatically
                             # It sets up layout and widgets that are defined
         self.pointcloudButton.clicked.connect(self.buildPointcloud)
+        self.selectDataButton.clicked.connect(self.openSelectData)
 
+
+    def openSelectData(self):
+        self.dialog = SelectDataWindow()
+        self.dialog.open()
 
 
     def buildPointcloud(self):
-        # Performed when pointcloudButton has been clicked
-        self.newThread = buildPointcloudThread("/home/ukito/Documents/inżynierka/sample/lms_front",
-                                                   "/home/ukito/Documents/inżynierka/sample/gps/ins.csv",
-                                                   "/home/ukito/Documents/inżynierka/robotcar-dataset-sdk-2.0.1/extrinsics",
+        dirDataF = self.dialog.chooseDataFolder.text()
+        dirDataF = dirDataF.replace('\r', "").replace('\n', "").replace('file://',"")
+        dirExtrF = self.dialog.chooseExtrFolder.text()
+        dirExtrF = dirExtrF.replace('\r', "").replace('\n', "").replace('file://', "")
+        dirModelsF = self.dialog.chooseModelsFolder.text()
+        dirModelsF = dirModelsF.replace('\r', "").replace('\n', "").replace('file://', "")
+        dirIns = dirDataF + "/gps/ins.csv"
+        dirLidar = dirDataF + "/" + self.dialog.chosseLidar.currentText()
+        #dirCamera = dirFolder + "/sample/" + self.dialog.chooseCamera.currentText()
+        startTime = self.dialog.startTime.text()
+        endTime = self.dialog.endTime.text()
+
+
+
+
+        #"/home/qahu/Documents/inz"
+        #"1418381798086398, 1418381817118734"
+        #int(startTime), int(endTime)
+
+        self.newThread = buildPointcloudThread(    str(dirLidar),
+                                                   str(dirIns),
+                                                   str(dirExtrF),
                                                    1418381798086398, 1418381817118734)
         self.connect(self.newThread, SIGNAL("drawPointcloud(PyQt_PyObject)"), self.drawPointcloud)
         self.newThread.start()
@@ -47,7 +79,6 @@ class Application(QtGui.QMainWindow, MainWindowDesign.Ui_MainWindow):
 
         self.pointcloudButton.setEnabled(True)
         self.pointcloudButton.setText("Build and draw sample pointcloud")
-
 
 
 
